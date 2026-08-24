9349a08dd8ca85bcd67889179109ff72d82006ea
flexult <touma.leandre.72@gmail.com>
2026-08-24 17:59:56 +0000

    patch

--- play/js/main.js
+++ play/js/main.js
@@ -136,7 +136,7 @@ submitBtn.addEventListener('click', () => {
     const result = evaluate(combinaison, essai);
     const resultCases = [...rows[turn].querySelectorAll('.result')];
     resultCases[0].innerText = result[0];
-    resultCases[1].inmerText = result[1];
+    resultCases[1].innerText = result[1];
 
     // Résoudre le jeu si l'utilisateur à trouvé
     if (result[0] === 4) {
